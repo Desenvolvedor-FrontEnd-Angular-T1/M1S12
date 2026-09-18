9e092af9dd7054b632e0edbb19d049f1a3279c77
Your Name <you@example.com>
2026-09-18 10:00:48 -0300

    Atividades Aula 3

---
+++ Aula_3/exemplo_cdn.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+  swal("Good job!", "You clicked the button!", "success");
+}, 2000);
+
+console.log(moment().format('dddd')); 
+console.log(moment('20/01/2020', 'DD/MM/YYYY').format('MM-DD-YYYY')); 
---
+++ Aula_3/exemplo_npm_install.js
@@ -0,0 +1,5 @@
+var prompt = require('prompt-sync')();
+
+var n = prompt('Qual o seu nome? ');
+
+console.log('Nome', n);
